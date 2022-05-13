a85711fcefff494ada932ecbd1649b18430ae184
LawssssCat <1191693505@qq.com>
2022-05-13 15:38:58 +0800

    base feature

--- ffmeg.js
+++ ffmeg.js
@@ -3,7 +3,7 @@ const child = require('child_process')
 const path = require('path');
 
 // 得到所有的路径
-function getJsonFiles(jsonPath) {
+function getJsonFiles() {
     let urlName = [];
 
     function findJsonFile(url) {
@@ -22,47 +22,93 @@ function getJsonFiles(jsonPath) {
             }
         });
     }
-    findJsonFile(jsonPath);
+    findJsonFile(config.assets);
     return urlName
 }
 
-function FFmeg(limp, name) {
-    child.execSync(limp, {maxBuffer: 1024*1024*1024}, function (err) {
+// 分析所有输出的路径
+function getJsonRes(urlName) {
+    let resList = [];
+
+    urlName.forEach(item => {
+        let fileinfo = getJsonFilesInfo(item);
+        resList.push(fileinfo);
+    })
+    return resList;
+}
+
+function FFmeg(info) {
+
+    if (!fs.existsSync(info.restUrl)) {
+        fs.mkdirSync(info.restUrl, { recursive: true });
+    }
+
+    function getComm(url) {
+        return fs.existsSync(url) ?  ` -i ${url} `: '';
+    }
+
+    let audioComm = getComm(`${info.assetUrl}/audio.m4s`);
+    let command = `ffmpeg -i ${info.assetUrl}/video.m4s `+audioComm+` -codec copy "${info.restUrl}/${info.restName}"`;
+    if(config.resOverwrite)
+        command+=' -y ';
+    console.log(command);
+    child.execSync(command, {maxBuffer: 1024*1024*1024}, function (err) {
         if (err) {
             console.log(err.message);
         } else {
-            console.log('成功合并视频:' + name);
+            console.log('ok');
         }
     })
 }
 
-// 获取视频的名字
-function getName(videoUrl) {
-    videoUrl = path.resolve(videoUrl, '../entry.json')
-    let rawdata = fs.readFileSync(videoUrl);
-    let videoInfo = JSON.parse(rawdata);
-    return videoInfo['page_data']['part']
+// 获取视频的信息
+function getJsonFilesInfo(assetUrl) {
+    let entryUrl = path.resolve(assetUrl, '../entry.json')
+    let rawdata = fs.readFileSync(entryUrl);
+    let fileinfo = JSON.parse(rawdata);
+
+    let info = {}
+    info.assetUrl = assetUrl;
+    let restUrl = config.res;
+    switch (config.vedioIdMode) {
+        case 'avid':
+            restUrl+=('/'+fileinfo['avid']);
+            break;
+        case 'title':
+            restUrl+=('/'+correctFilename(fileinfo['title']));
+            break;
+        default:
+            Error('error config.vedioIdMode \''+config.vedioIdMode+'\'');
+            break;
+    }
+    info.restUrl = restUrl;
+    info.restName = correctFilename(fileinfo['page_data']['part'])+'.mp4';
+
+    return info;
 }
 
-
-function makeDir(urlName) {
-    let resList = []
-    urlName.forEach(item => {
-        videoUrl = path.resolve(item.replace('assets', 'res'), '../../')
-        resList.push(videoUrl)
-    })
-    resList = Array.from(new Set(resList))
-    resList.forEach(item => {
-        if (!fs.existsSync(item)) {
-            fs.mkdirSync(item)
-        }
-    })
+// 去除字符串中的空格
+function correctFilename(str) {
+    str = str.replace(/^\s*|\s*$/g, '');
+    // if(str.indexOf(' ') >= 0) 
+    //     str = '\''+str+'\'';
+    return str;
 }
 
+function init(newconfig) {
+    Object.assign(config, newconfig);
+}
+
+let config = {
+    assets:'./assets',        // 待合并资源位置
+    res:'./res',              // 合并后视频位置
+    resOverwrite:true,
+    vedioIdMode:'avid'       // avid=以avid为目录，title=以title为目录
+}
 
 module.exports = {
+    init,
     FFmeg,
     getJsonFiles,
-    makeDir,
-    getName
+    getJsonRes,
 }
--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 const ffmeg = require('./ffmeg')
 const fs = require('fs');
 
-
 function main() {
-    let urlName = ffmeg.getJsonFiles('./assets')
-    // 创建目录
-    ffmeg.makeDir(urlName)
-    urlName.forEach(item => {
-        let filename = ffmeg.getName(item)
-        filename = item.replace('assets', 'res').substring(0, 15) + '/' + filename
-        filename = filename.replace(/\s*/g,"");
-        if (!fs.existsSync(filename)) {
-            let url = `FFmpeg -i ${item}/video.m4s -i ${item}/audio.m4s -codec copy ${filename}.mp4`
-            ffmeg.FFmeg(url, item)
-        }
-    })
+    let config = {
+        vedioIdMode: 'title'
+    }
+    ffmeg.init(config);
+    // 待处理资源
+    let urlAsset = ffmeg.getJsonFiles();
+    // 输出路径
+    let urlRes = ffmeg.getJsonRes(urlAsset);
+    // 处理
+    urlRes.forEach(item => {
+        ffmeg.FFmeg(item);
+    });
 }
 
-
-
 function init() {
-    console.log('合成后的视频统一在 res 目录下,以视频id为目录.');
     main()
     console.log('已合成全部视频');
 }
